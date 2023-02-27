6f4b7dfcb0d92ada1c32351c128afcbe11f4a852
David Yu <Davyie@n130-p110.eduroam.kth.se>
2023-02-27 19:46:17 +0100

    Extract constants from classes

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,8 @@
 class constants:
-  labels = {1: 'mycket negativt', 2: 'negativt', 3: 'neutral', 4: 'positivt', 5: 'mycket positivt' }
-  
+  '''
+    This class contains constants for the project 
+  '''
+  absa_labels = {1: 'mycket negativt', 2: 'negativt', 3: 'neutral', 4: 'positivt', 5: 'mycket positivt' }
+  ski_labels = {1: 'image', 2: 'product quality', 3: 'service quality', 4: 'expectation'}
+
+  absa_file_path = '../dataset/absabank_imm/P_annotation.tsv'
