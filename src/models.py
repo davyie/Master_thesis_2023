221cd5c92f3d1aee2d02b6abcec208cd71b12e85
David Yu <Davyie@n130-p110.eduroam.kth.se>
2023-03-06 16:28:13 +0100

    Add fine tuning method. Add extra output to process()

--- src/models.py
+++ src/models.py
@@ -1,5 +1,5 @@
 import torch 
-from transformers import AutoModel, AutoTokenizer, AdamW, AutoModelForMaskedLM
+from transformers import AutoModel, AutoTokenizer, AdamW, AutoModelForMaskedLM, AlbertTokenizer, AlbertModel, AlbertForMaskedLM
 import numpy as np
 from tqdm import tqdm
 from constants import constants
@@ -12,16 +12,24 @@ class Models:
       This class contains different models we can use to obtain the embeddings 
     '''
     self.model_name = model_name
-    self.tok = AutoTokenizer.from_pretrained(model_name)
-    self.model = AutoModelForMaskedLM.from_pretrained(model_name)
+    if model_name == constants.KBLab_albert:
+      self.tok = AlbertTokenizer.from_pretrained(model_name)
+    else: 
+      self.tok = AutoTokenizer.from_pretrained(model_name)
     return 
   
   def process(self, data): 
     '''
       This method will process the data and obtain embeddings 
     '''
+
+    if self.model_name == constants.KBLab_albert:
+      model = AlbertModel.from_pretrained(self.model_name)
+    else:
+      model = AutoModel.from_pretrained(self.model_name)
+    
     inputs = self.tokenize_text(data)
-    return self.model(**inputs)
+    return model(**inputs), inputs
   
   def decode(self, token_id):
     return self.tok.decode(token_id)
@@ -35,6 +43,7 @@ class Models:
       i.e. 'this is a sentence' or ['this is sentence', 'This is another sentence']
     '''
     return self.tok(text, padding=pad, truncation=truncate, max_length=max_len, return_tensors="pt")
+    # return self.tok(text, return_tensors="pt")
 
   def preprocess_maskedLM_dataset(self, text_data, mask_percentage=0.15, batch_size=8):
     '''
@@ -55,16 +64,27 @@ class Models:
     dataloader = torch.utils.data.DataLoader(tokenized_dataset, batch_size=batch_size, shuffle=True)
     return dataloader
 
-  def fine_tune(self, text_data, epochs):
+  def fine_tune_MLM(self, text_data, epochs, is_save=False):
     '''
       This method contains training loop for the model. 
+      First it inits the model and then process the data. 
+      This works for 
+      Lastly, it runs the training loop and save the model if its set to save. 
       @param text_data - a list of strings 
       @param epochs - the number of epochs we train the model 
     '''
+    # Load in the model. Tokenizer is already loaded in 
+    if self.model_name == constants.KBLab_albert:
+      model = AlbertForMaskedLM.from_pretrained(self.model_name)
+    else: 
+      model = AutoModel.from_pretrained(self.model_name)
+
+    model.train() # Turn on training state
+
     # Prepare dataset 
     dataloader = self.preprocess_maskedLM_dataset(text_data, 0.15)
-    self.model.train()
-    optimizer = AdamW(self.model.parameters(), lr=1e-5)
+    
+    optimizer = AdamW(model.parameters(), lr=1e-5)
 
     for epoch in range(epochs):
       loop = tqdm(dataloader)
@@ -73,12 +93,15 @@ class Models:
         input_ids = batch['input_ids']
         attention_mask = batch['attention_mask']
         labels = batch['labels']
-        outputs = self.model(input_ids, attention_mask=attention_mask, labels=labels)
+        outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
         loss = outputs[0]
         loss.backward()
         optimizer.step()
 
         loop.set_description('Epoch: {}'.format(epoch))
         loop.set_postfix(loss=loss.item)
-    model_name = self.model_name + "_finetuned"
-    self.model.save_pretrained("./models/" + model_name)
+    if is_save:
+      model.eval()
+      self.model_name = self.model_name + "_finetuned"
+      model.save_pretrained("./models/" + self.model_name)
+      print("./models/" + self.model_name)
