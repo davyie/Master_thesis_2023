f4a1e3cf4a9cb292f57d13fc9f43bf583d4e991e
David Yu <Davyie@n130-p110.eduroam.kth.se>
2023-03-08 11:49:27 +0100

    Add experiments

--- src/experiment.py
+++ src/experiment.py
@@ -1,7 +1,44 @@
-from models import Models
-from data_loader import DataLoader
+from clustering import Clustering
+from constants import constants
+from metric import Metric
+from utils import Utils
 
 class Experiment: 
-  def __init__(self, path_to_data, model_name) -> None:
-    self.model = Models(model_name=model_name)
-    self.data_loader = DataLoader(path=path_to_data)
+  def KB_BERT_Experiment_BASE(model_name, tokenizer_name, path_to_data):
+    data, true_labels, cluster_labels = Clustering.cluster_bert(model_name, tokenizer_name, path_to_data)
+    M = Metric(data, true_labels=true_labels, cluster_labels=cluster_labels)
+    # print('Without improvement: ', M.compute_accuracy(), M.compute_precision(), M.compute_recall(), M.compute_silhouette(), M.compute_calinski_harabasz_score())
+    metrics = M.compute_all()
+    Utils.print_to_file("result/KB_BERT_BASE.txt", metrics)
+  
+  def KB_BERT_Experiment_IMPROV():
+    data, true_labels, cluster_labels = Clustering.cluster_bert(constants.KBLab_bert, constants.absa_file_path, is_initial_points=True)
+    M = Metric(data, true_labels=true_labels, cluster_labels=cluster_labels)
+    metrics = M.compute_all()
+    Utils.print_to_file("result/KB_BERT_IMPROV.txt", metrics)
+
+  def AF_BERT_Experiment_BASE():
+    data, true_labels, cluster_labels = Clustering.cluster_bert(constants.AF_bert, constants.absa_file_path)
+    M = Metric(data, true_labels=true_labels, cluster_labels=cluster_labels)
+    # print('Without improvement: ', M.compute_accuracy(), M.compute_precision(), M.compute_recall(), M.compute_silhouette(), M.compute_calinski_harabasz_score())
+    metrics = M.compute_all()
+    Utils.print_to_file("result/AF_BERT_BASE.txt", metrics)
+
+  def AF_BERT_Experiment_IMPROV():
+    data, true_labels, cluster_labels = Clustering.cluster_bert(constants.AF_bert, constants.absa_file_path, is_initial_points=True)
+    M = Metric(data, true_labels=true_labels, cluster_labels=cluster_labels)
+    metrics = M.compute_all()
+    Utils.print_to_file("result/AF_BERT_IMPROV.txt", metrics)
+
+  def ML_BERT_Experiment_BASE():
+    data, true_labels, cluster_labels = Clustering.cluster_bert(constants.ML_bert, constants.absa_file_path)
+    M = Metric(data, true_labels=true_labels, cluster_labels=cluster_labels)
+    # print('Without improvement: ', M.compute_accuracy(), M.compute_precision(), M.compute_recall(), M.compute_silhouette(), M.compute_calinski_harabasz_score())
+    metrics = M.compute_all()
+    Utils.print_to_file("result/ML_BERT_BASE.txt", metrics)
+
+  def ML_BERT_Experiment_IMPROV():
+    data, true_labels, cluster_labels = Clustering.cluster_bert(constants.ML_bert, constants.absa_file_path, is_initial_points=True)
+    M = Metric(data, true_labels=true_labels, cluster_labels=cluster_labels)
+    metrics = M.compute_all()
+    Utils.print_to_file("result/ML_BERT_IMPROV.txt", metrics)
