c511c5d18a28156ddcaa255af01b120d93fb465d
David Yu <Davyie@n130-p110.eduroam.kth.se>
2023-03-15 20:01:40 +0100

    Add experiment

--- src/experiment.py
+++ src/experiment.py
@@ -2,43 +2,70 @@ from clustering import Clustering
 from constants import constants
 from metric import Metric
 from utils import Utils
+import pickle
 
 class Experiment: 
-  def KB_BERT_Experiment_BASE(model_name, tokenizer_name, path_to_data):
-    data, true_labels, cluster_labels = Clustering.cluster_bert(model_name, tokenizer_name, path_to_data)
-    M = Metric(data, true_labels=true_labels, cluster_labels=cluster_labels)
-    # print('Without improvement: ', M.compute_accuracy(), M.compute_precision(), M.compute_recall(), M.compute_silhouette(), M.compute_calinski_harabasz_score())
-    metrics = M.compute_all()
-    Utils.print_to_file("result/KB_BERT_BASE.txt", metrics)
-  
-  def KB_BERT_Experiment_IMPROV():
-    data, true_labels, cluster_labels = Clustering.cluster_bert(constants.KBLab_bert, constants.absa_file_path, is_initial_points=True)
-    M = Metric(data, true_labels=true_labels, cluster_labels=cluster_labels)
-    metrics = M.compute_all()
-    Utils.print_to_file("result/KB_BERT_IMPROV.txt", metrics)
+  def run_experiment_base():
+    is_interval = True
+    metrics = {}
 
-  def AF_BERT_Experiment_BASE():
-    data, true_labels, cluster_labels = Clustering.cluster_bert(constants.AF_bert, constants.absa_file_path)
+    data, true_labels, cluster_labels = Clustering.cluster_bert(constants.KB_bert_finetuned, constants.KB_bert, constants.absa_file_path, is_interval=is_interval)
     M = Metric(data, true_labels=true_labels, cluster_labels=cluster_labels)
-    # print('Without improvement: ', M.compute_accuracy(), M.compute_precision(), M.compute_recall(), M.compute_silhouette(), M.compute_calinski_harabasz_score())
-    metrics = M.compute_all()
-    Utils.print_to_file("result/AF_BERT_BASE.txt", metrics)
+    metrics['KB bert'] = M.compute_all()
 
-  def AF_BERT_Experiment_IMPROV():
-    data, true_labels, cluster_labels = Clustering.cluster_bert(constants.AF_bert, constants.absa_file_path, is_initial_points=True)
+    data, true_labels, cluster_labels = Clustering.cluster_albert(constants.KB_albert_finetuned, constants.KB_albert, constants.absa_file_path, is_interval=is_interval)
     M = Metric(data, true_labels=true_labels, cluster_labels=cluster_labels)
-    metrics = M.compute_all()
-    Utils.print_to_file("result/AF_BERT_IMPROV.txt", metrics)
+    metrics['KB albert'] = M.compute_all()
 
-  def ML_BERT_Experiment_BASE():
-    data, true_labels, cluster_labels = Clustering.cluster_bert(constants.ML_bert, constants.absa_file_path)
+    data, true_labels, cluster_labels = Clustering.cluster_sbert(constants.KB_sbert_finetuned, constants.KB_sbert, constants.absa_file_path, is_interval=is_interval)
     M = Metric(data, true_labels=true_labels, cluster_labels=cluster_labels)
-    # print('Without improvement: ', M.compute_accuracy(), M.compute_precision(), M.compute_recall(), M.compute_silhouette(), M.compute_calinski_harabasz_score())
-    metrics = M.compute_all()
-    Utils.print_to_file("result/ML_BERT_BASE.txt", metrics)
+    metrics['KB sbert'] = M.compute_all()
 
-  def ML_BERT_Experiment_IMPROV():
-    data, true_labels, cluster_labels = Clustering.cluster_bert(constants.ML_bert, constants.absa_file_path, is_initial_points=True)
+    data, true_labels, cluster_labels = Clustering.cluster_bert(constants.AF_bert_finetuned, constants.AF_bert, constants.absa_file_path, is_interval=is_interval)
     M = Metric(data, true_labels=true_labels, cluster_labels=cluster_labels)
-    metrics = M.compute_all()
-    Utils.print_to_file("result/ML_BERT_IMPROV.txt", metrics)
+    metrics['AF bert'] = M.compute_all()
+
+    data, true_labels, cluster_labels = Clustering.cluster_bert(constants.ML_bert_finetuned, constants.ML_bert, constants.absa_file_path, is_interval=is_interval)
+    M = Metric(data, true_labels=true_labels, cluster_labels=cluster_labels)
+    metrics['ML bert'] = M.compute_all()
+
+    data, true_labels, cluster_labels = Clustering.cluster_sbert(constants.ML_sbert_finetuned, constants.ML_sbert, constants.absa_file_path, is_interval=is_interval)
+    M = Metric(data, true_labels=true_labels, cluster_labels=cluster_labels)
+    metrics['ML sbert'] = M.compute_all()
+    
+    print(metrics)
+    file = open('eval_metrics_base.pkl', 'wb')
+    pickle.dump(metrics, file)
+    file.close()
+
+  def run_experiment_improve():
+    is_interval = False
+    metrics = {}
+    data, true_labels, cluster_labels = Clustering.cluster_bert(constants.KB_bert_finetuned, constants.KB_bert, constants.absa_file_path, is_interval=is_interval, is_improve=True)
+    M = Metric(data, true_labels=true_labels, cluster_labels=cluster_labels)
+    metrics['KB bert'] = M.compute_all()
+
+    data, true_labels, cluster_labels = Clustering.cluster_albert(constants.KB_albert_finetuned, constants.KB_albert, constants.absa_file_path, is_interval=is_interval, is_improve=True)
+    M = Metric(data, true_labels=true_labels, cluster_labels=cluster_labels)
+    metrics['KB albert'] = M.compute_all()
+
+    data, true_labels, cluster_labels = Clustering.cluster_sbert(constants.KB_sbert_finetuned, constants.KB_sbert, constants.absa_file_path, is_interval=is_interval, is_improve=True)
+    M = Metric(data, true_labels=true_labels, cluster_labels=cluster_labels)
+    metrics['KB sbert'] = M.compute_all()
+
+    data, true_labels, cluster_labels = Clustering.cluster_bert(constants.AF_bert_finetuned, constants.AF_bert, constants.absa_file_path, is_interval=is_interval, is_improve=True)
+    M = Metric(data, true_labels=true_labels, cluster_labels=cluster_labels)
+    metrics['AF bert'] = M.compute_all()
+
+    data, true_labels, cluster_labels = Clustering.cluster_bert(constants.ML_bert_finetuned, constants.ML_bert, constants.absa_file_path, is_interval=is_interval, is_improve=True)
+    M = Metric(data, true_labels=true_labels, cluster_labels=cluster_labels)
+    metrics['ML bert'] = M.compute_all()
+
+    data, true_labels, cluster_labels = Clustering.cluster_sbert(constants.ML_sbert_finetuned, constants.ML_sbert, constants.absa_file_path, is_interval=is_interval, is_improve=True)
+    M = Metric(data, true_labels=true_labels, cluster_labels=cluster_labels)
+    metrics['ML sbert'] = M.compute_all()
+    
+    print(metrics)
+    file = open('eval_metrics_improv.pkl', 'wb')
+    pickle.dump(metrics, file)
+    file.close()
