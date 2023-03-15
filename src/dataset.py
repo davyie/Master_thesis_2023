9427bc9f50dc818326269e31b84bc243f5ebdd3c
David Yu <Davyie@n130-p110.eduroam.kth.se>
2023-03-15 19:55:54 +0100

    Remove token_type_id for finetuneing

--- src/dataset.py
+++ src/dataset.py
@@ -7,12 +7,12 @@ class Dataset(torch.utils.data.Dataset):
         return len(self.encodings)
       def __getitem__(self, index):
         input_ids = torch.Tensor(self.encodings['input_ids'][index])
-        token_type_ids = torch.Tensor(self.encodings['token_type_ids'][index])
+        # token_type_ids = torch.Tensor(self.encodings['token_type_ids'][index])
         attention_mask = torch.Tensor(self.encodings['attention_mask'][index])
         labels = torch.Tensor(self.encodings['labels'][index])
         return {
           'input_ids': input_ids,
-          'token_type_ids': token_type_ids,
+          # 'token_type_ids': token_type_ids,
           'attention_mask': attention_mask,
           'labels': labels
         }
